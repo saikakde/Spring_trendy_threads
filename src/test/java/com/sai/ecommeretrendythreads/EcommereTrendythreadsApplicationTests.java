ded208067fcce61f00f5d4b263e6f1c10c745dd7
saikakde <saikakde121@gmail.com>
2023-10-06 13:31:32 +0530

    first commit

---
+++ src/test/java/com/sai/ecommeretrendythreads/EcommereTrendythreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.ecommeretrendythreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommereTrendythreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
